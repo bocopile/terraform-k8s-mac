60eecb405e3e92fedde34953e4b150aeef7e7da8
bocopile <gjrjr4545@gmail.com>
2025-11-14 16:49:16 +0900

    [join-all.sh 수정] - master node, worker node join 수정

--- shell/join-all.sh
+++ shell/join-all.sh
@@ -6,13 +6,13 @@ multipass transfer k8s-master-0:/home/ubuntu/join.sh ./shell/join.sh
 multipass transfer k8s-master-0:/home/ubuntu/join-controlplane.sh ./shell/join-controlplane.sh
 
 # Control Plane Join (1, 2)
-#for i in 1 2; do
-#  multipass transfer ./shell/join-controlplane.sh k8s-master-${i}:/home/ubuntu/join-controlplane.sh
-#  multipass exec k8s-master-${i} -- bash -c "chmod +x /home/ubuntu/join-controlplane.sh && sudo bash /home/ubuntu/join-controlplane.sh"
-#done
+for i in 1 2; do
+  multipass transfer ./shell/join-controlplane.sh k8s-master-${i}:/home/ubuntu/join-controlplane.sh
+  multipass exec k8s-master-${i} -- bash -c "chmod +x /home/ubuntu/join-controlplane.sh && sudo bash /home/ubuntu/join-controlplane.sh"
+done
 
 # Worker Join (0 ~ 5)
-for i in {0..1}; do
+for i in {0..5}; do
   multipass transfer ./shell/join.sh k8s-worker-${i}:/home/ubuntu/join.sh
   multipass exec k8s-worker-${i} -- bash -c "chmod +x /home/ubuntu/join.sh && sudo bash /home/ubuntu/join.sh"
 done
